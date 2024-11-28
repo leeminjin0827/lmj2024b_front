87912f8055609528580378dfcb894800f78cbd98
leeminjin0827 <lmj98202@gmail.com>
2024-11-28 13:21:07 +0900

    day14 중간과제8

--- day12/5_중간과제.js
+++ day12/5_중간과제.js
@@ -1,20 +1,20 @@
 // 문제 1
 
-let movieName = [ '히든페이스' , '워키드' , '글래디에이터2' , '청설' ]
-let movieRating = [ 8 , 4 , 7 , 6 ]
-for( let i = 0 ; i < movieName.length ; i++ ){
-    let name = movieName[i] ;
-    let rating = movieRating[i] ;
-    let star = '';
-    for( let j = 0 ; j < 10 ; j++ ){
-        if( j < rating ){
-            star += '★'
-        }else{
-            star += '☆'
-        }
-    }
-    document.write(name+star+'<br>')
-}
+// let movieName = [ '히든페이스' , '워키드' , '글래디에이터2' , '청설' ]
+// let movieRating = [ 8 , 4 , 7 , 6 ]
+// for( let i = 0 ; i < movieName.length ; i++ ){
+//     let name = movieName[i] ;
+//     let rating = movieRating[i] ;
+//     let star = '';
+//     for( let j = 0 ; j < 10 ; j++ ){
+//         if( j < rating ){
+//             star += '★'
+//         }else{
+//             star += '☆'
+//         }
+//     }
+//     document.write(name+star+'<br>')
+// }
 
 let ovieName = [ '히든페이스' , '워키드' , '글래디에이터2' , '청설' ]
 let ovieRating = [ 8 , 4 , 7 , 6 ]
